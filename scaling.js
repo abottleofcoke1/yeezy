5a40baa8718452ae680326a74f527f24aeea5ccc
matt <mattyysekk@gmail.com>
2020-12-21 15:39:56 +0100

    Create scaling.js

---
+++ scaling.js
@@ -0,0 +1,10 @@
+if (screen.width >= 1680) {
+	
+	document.write("<style>body{max-width: 90%;}.box{margin-left: 458px; margin-top: 134px;}.cart{margin-left: 709px;}.cartSpace{margin-top: 43px;}.cart:hover{margin-left: 693px;}</style>")
+}
+
+function changeBackground(id)
+{
+ 
+  document.getElementById(id).style.backgroundColor = "#1c1c1c";
+}
